57d7544f5ca775760f286dfe18e2cf8186bef3f3
nwang <wangnanzs@gmail.com>
2019-05-21 00:57:21 -0700

    update to Peilianke

--- src/laioffer/CrossTraining2.java
+++ src/laioffer/CrossTraining2.java
@@ -1,7 +1,38 @@
 package laioffer;
 import java.util.*;
 public class CrossTraining2 {
-
+	public static void main(String[] args) {
+		GraphNode node1 = new GraphNode(1);
+		GraphNode node2 = new GraphNode(2);
+		GraphNode node3 = new GraphNode(3);
+		GraphNode node4 = new GraphNode(4);
+		node1.neighbors = new ArrayList<>();
+//		node1.neighbors.add(node2);
+		node1.neighbors.add(node3);
+		node2.neighbors = new ArrayList<>();
+//		node2.neighbors.add(node1);
+//		node2.neighbors.add(node3);
+//		node2.neighbors.add(node4);
+		node3.neighbors = new ArrayList<>();
+		node3.neighbors.add(node1);
+//		node3.neighbors.add(node2);
+		node3.neighbors.add(node4);
+		node4.neighbors = new ArrayList<>();
+//		node4.neighbors.add(node2);
+		node4.neighbors.add(node3);
+		List<GraphNode> graph = new ArrayList<>();
+		graph.add(node1);
+		graph.add(node2);
+		graph.add(node3);
+		graph.add(node4);
+		CT2Solution sol = new CT2Solution();
+		List<GraphNode> newGraph = sol.copy1(graph);
+		for(GraphNode node: newGraph) {
+			System.out.println(node.key);
+		}
+		
+		
+	}
 }
 
 //class GraphNode{
@@ -13,6 +44,62 @@ public class CrossTraining2 {
 //	}
 //}
 class CT2Solution{
+	private void bfs(GraphNode node, Queue<GraphNode> queue, 
+			Map<GraphNode,GraphNode> map, List<GraphNode> result) {
+		GraphNode copied = new GraphNode(node.key);
+		map.put(node,copied);
+		queue.offer(node);
+		while(!queue.isEmpty()) {
+			GraphNode element = queue.poll();
+			GraphNode elementCopy = map.get(element);
+			result.add(elementCopy);
+			for(GraphNode child :element.neighbors) {
+				GraphNode childCopy = map.get(child);
+				if(childCopy == null) {
+					childCopy = new GraphNode(child.key);
+					map.put(child,childCopy);
+					queue.offer(child);
+				}
+				elementCopy.neighbors.add(childCopy);
+			}
+		}
+	}
+	public List<GraphNode> copy1(List<GraphNode> graph){
+		Map<GraphNode,GraphNode> map = new HashMap<>();
+		List<GraphNode> res = new ArrayList<>();
+		for(GraphNode node : graph) {
+			if(!map.containsKey(node)) {
+				cloneNodes(node,map,res);
+			}
+		}
+		return res;
+	}
+	// Using BFS
+	private void cloneNodes(GraphNode input, Map<GraphNode, GraphNode> map, List<GraphNode> ls) {
+		if(input == null) {
+			return;
+		}
+		Queue<GraphNode> queue = new ArrayDeque<>();
+		GraphNode copied = new GraphNode(input.key);
+		map.put(input, copied);
+		ls.add(copied);
+		queue.offer(input);
+		while(!queue.isEmpty()) {
+			GraphNode curr = queue.poll();
+			GraphNode copiedCurr = map.get(curr);
+			for(GraphNode node: curr.neighbors) {
+				if(!map.containsKey(node)) {
+					GraphNode copiedNode = new GraphNode(node.key);
+					ls.add(copiedNode);
+					map.put(node,copiedNode);
+					queue.offer(node);
+				}
+				copiedCurr.neighbors.add(map.get(node));
+			}
+		}
+		return;
+	}
+	
 	public List<GraphNode> copy(List<GraphNode> graph){
 		Map<GraphNode,GraphNode> map = new HashMap<>();
 		List<GraphNode> res = new ArrayList<>();
@@ -23,6 +110,7 @@ class CT2Solution{
 		}
 		return res;
 	}
+	// Using DFS
 	private GraphNode cloneNode(GraphNode input, Map<GraphNode, GraphNode> map, List<GraphNode> ls) {
 		if(input == null) {
 			return input;
@@ -36,6 +124,6 @@ class CT2Solution{
 		for(GraphNode node: input.neighbors) {
 			newNode.neighbors.add(cloneNode(node,map,ls));
 		}
-		return map.get(input);
+		return newNode;
 	}
 }
--- src/laioffer/PeiLianKe.java
+++ src/laioffer/PeiLianKe.java
@@ -1,14 +1,106 @@
 package laioffer;
 import java.util.*;
 public class PeiLianKe {
-	
+	public static void main(String[] args) {
+		Integer[] array = new Integer[] {5,2,10,1,3,8,13,null,null,null,4,null,null,11,15};
+//		TreeNode root = TreeGenerator.generate(array);
+		PLKSolution plk = new PLKSolution();
+		TreeNode root = plk.reconstruct(new int[] {1,3,2,5,7,6,4});
+		root = plk.convertToList(root);
+		while(root!=null) {
+			System.out.print(root.val+" ");
+			root = root.right;
+		}
+	}
 }
 
 class KTreeNode{
 	int val;
 	List<KTreeNode> children;
 }
+
 class PLKSolution{
+	public TreeNode reconstruct(int[] post) {
+	    // Write your solution here
+	    int[] in = new int[post.length];
+	    for(int i=0;i<in.length;i++){
+	        in[i] = post[i];
+	    }
+	    Arrays.sort(in);
+	    Map<Integer,Integer> inMap = new HashMap<>();
+	    for(int i=0;i<in.length;i++){
+	        inMap.put(in[i],i);
+	    }
+	    return reconstruct(post,0,post.length-1,inMap);
+	}
+	private TreeNode reconstruct(int[] post, int left, int right, Map<Integer,Integer> inMap){
+	    if(left > right){
+	        return null;
+	    }
+	    TreeNode root = new TreeNode(post[right]);
+	    int leftSize = inMap.get(post[right])-inMap.get(post[left]);
+	    root.left = reconstruct(post,left,left+leftSize-1,inMap);
+	    root.right = reconstruct(post,left+leftSize,right-1,inMap);
+	    return root;
+	}
+	public TreeNode construct(int[] inOrder, int[] preOrder) {
+		Map<Integer,Integer> inMap = new HashMap<>();
+		for(int i=0;i<inOrder.length;i++) {
+			inMap.put(inOrder[i],i);
+		}
+		return construct(inOrder,0,inOrder.length-1,preOrder,0,preOrder.length-1,inMap);
+	}
+	private TreeNode construct(int[] inOrder, int inLeft, int inRight,
+							   int[] preOrder, int preLeft, int preRight,
+							   Map<Integer,Integer> inMap) {
+		// Base Case
+		if(preLeft > preRight) {
+			return null;
+		}
+		TreeNode root = new TreeNode(preOrder[preLeft]);
+		int rootIndex = inMap.get(preOrder[preLeft]);
+		int leftSize = rootIndex-inLeft;
+		root.left = construct(inOrder,inLeft,inLeft+leftSize-1,preOrder,preLeft+1,preLeft+leftSize,inMap);
+		root.right = construct(inOrder,rootIndex+1,inRight,preOrder,preLeft+leftSize+1,preRight,inMap);
+		return root;
+	}
+	
+	public TreeNode convertToList(TreeNode root) {
+		TreeNode[] res = new TreeNode[2];// res[0] is head; res[1] is previous
+		convertInOrder(root,res);
+		return res[0];
+	}
+	private void convertInOrder(TreeNode root, TreeNode[] res) {
+		if(root == null) {
+			return;
+		}
+		convertInOrder(root.left,res);
+		if(res[1] == null) {
+			res[0] = root;
+		}else {
+			res[1].right = root;
+			root.left = res[1];
+		}
+		res[1] = root;
+		convertInOrder(root.right,res);
+	}
+	private void convertPreOrder(TreeNode root, TreeNode[] res) {
+		if(root == null) {
+			return;
+		}
+		TreeNode leftChild = root.left;
+		TreeNode rightChild = root.right;
+		if(res[0] == null) {
+			res[0] = root;
+		}
+		if(res[1] != null) {
+			res[1].right = root;
+			root.left = res[1];
+		}
+		res[1] = root;
+		convertPreOrder(leftChild,res);
+		convertPreOrder(rightChild,res);
+	}
 	public List<Integer> zigZag(TreeNode root) {
 	    // Write your solution here
 	    // Similar to level-order traversal but with a Deque data structure
